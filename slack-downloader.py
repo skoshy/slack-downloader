20d87fa01bad1577ae256bc2f18096687ed2cf73
Stefan Koshy <noreply@skoshy.com>
2018-11-28 11:38:17 -0500

    Added support for a JSON file config

--- slack-downloader.py
+++ slack-downloader.py
@@ -18,22 +18,19 @@ import time
 from datetime import datetime, timedelta
 from pprint import pprint # for debugging purposes
 
-# --- --- --- --- ---
-# CONFIGURATION BEGIN
-# --- --- --- --- ---
+################################################################
+################################################################
+################################################################
 
-# API Token: see https://api.slack.com/custom-integrations/legacy-tokens
-TOKEN = "<your_token>"
-
-# Set Token from environment variable SLACK_TOKEN (if it exists)
-if 'SLACK_TOKEN' in os.environ:
-	TOKEN = os.environ['SLACK_TOKEN']
+# Instatiate vars, they'll be populated in the main method
+TOKEN = ""
+CHANNELS = []
 
 # output main directory, without slashes
 OUTPUTDIR = "data"
 
 # enable debug?
-DEBUG = False
+DEBUG = True
 
 # enable extremely verbose debug?
 EXTREME_DEBUG = False
@@ -52,6 +49,12 @@ MAINDIR = os.path.dirname(os.path.realpath(__file__))+'/'
 
 # useful to avoid duplicate downloads
 TIMESTAMPFILE = MAINDIR+"offset.txt"
+CONFIGFILE = MAINDIR+"config.json"
+
+# Memos
+MEMO_USERS = {}
+MEMO_CHANNELS = {}
+MEMO_GROUPS = {}
 
 # format a response in json format
 def response_to_json(response):
@@ -63,13 +66,36 @@ def response_to_json(response):
 		return response.json()
 
 # file renaming function
-def get_local_filename(basedir, date, filename, user):
+def get_local_filename(basedir, filename, user, id, timestamp):
 	# converting date from epoch time to readable format
-	date = time.strftime('%Y%m%d_%H%M%S', time.localtime(float(date)))
+	date = convert_timestamp_to_date(timestamp)
 	# splitting filename to file extension
 	filename, file_extension = os.path.splitext(filename)
 	# retrieving full filename with path and returning it
-	return basedir+'/'+str(date)+'-'+filename+'_by_'+user+file_extension
+	return basedir+'/'+str(timestamp)+'-'+id+file_extension
+
+def convert_timestamp_to_date(timestamp):
+	return time.strftime('%Y%m%d_%H%M%S', time.localtime(float(timestamp)))
+
+# get saved timestamp of last download
+# API Token: see https://api.slack.com/custom-integrations/legacy-tokens
+def get_config():
+	global TOKEN, CHANNELS
+
+	try:
+		in_file = open(CONFIGFILE,"r")
+		text = in_file.read()
+		in_file.close()
+		configJson = json.loads(text)
+
+		# Now let's populate vars
+		TOKEN = configJson['token']
+		if 'SLACK_TOKEN' in os.environ:
+			TOKEN = os.environ['SLACK_TOKEN'] # override slack token if passed in as environ var
+
+		CHANNELS = configJson["channels"]
+	except Exception, e:
+		if DEBUG: print str(e)
 
 # save the timestamp of the last download (+1), in order to avoid duplicate downloads
 def set_timestamp(ts):
@@ -112,27 +138,45 @@ def download_file(url, local_filename, basedir):
 
 # get channel name from identifier
 def get_channel_name(id):
-	url = API+'/channels.info'
-	data = {'token': TOKEN, 'channel': id }
-	response = requests.post(url, data=data)
-	if DEBUG and EXTREME_DEBUG: pprint(response_to_json(response))
-	return response_to_json(response)['channel']['name']
+	if id in MEMO_CHANNELS:
+		responseAsJson = MEMO_CHANNELS[id]
+		if DEBUG and EXTREME_DEBUG: print('Channel in Memo - '+responseAsJson['channel']['name']+' - '+id)
+	else:
+		url = API+'/channels.info'
+		data = {'token': TOKEN, 'channel': id }
+		response = requests.post(url, data=data)
+		responseAsJson = response_to_json(response)
+		MEMO_CHANNELS[id] = responseAsJson
+	if DEBUG and EXTREME_DEBUG: pprint(responseAsJson)
+	return responseAsJson['channel']['name']
 
 # get group name from identifier
 def get_group_name(id):
-	url = API+'/groups.info'
-	data = {'token': TOKEN, 'channel': id }
-	response = requests.post(url, data=data)
-	if DEBUG and EXTREME_DEBUG: pprint(response_to_json(response))
-	return response_to_json(response)['group']['name']
+	if id in MEMO_GROUPS:
+		responseAsJson = MEMO_GROUPS[id]
+		if DEBUG and EXTREME_DEBUG: print('Group in Memo - '+responseAsJson['group']['name']+' - '+id)
+	else:
+		url = API+'/groups.info'
+		data = {'token': TOKEN, 'channel': id }
+		response = requests.post(url, data=data)
+		responseAsJson = response_to_json(response)
+		MEMO_GROUPS[id] = responseAsJson
+	if DEBUG and EXTREME_DEBUG: pprint(responseAsJson)
+	return responseAsJson['group']['name']
 
 # get user name from identifier
 def get_user_name(id):
-	url = API+'/users.info'
-	data = {'token': TOKEN, 'user': id }
-	response = requests.post(url, data=data)
-	if DEBUG and EXTREME_DEBUG: pprint(response_to_json(response))
-	return response_to_json(response)['user']['name']
+	if id in MEMO_USERS:
+		responseAsJson = MEMO_USERS[id]
+		if DEBUG and EXTREME_DEBUG: print('User in Memo - '+responseAsJson['user']['name']+' - '+str(id))
+	else:
+		url = API+'/users.info'
+		data = {'token': TOKEN, 'user': id }
+		response = requests.post(url, data=data)
+		responseAsJson = response_to_json(response)
+		MEMO_USERS[id] = responseAsJson
+	if DEBUG and EXTREME_DEBUG: pprint(responseAsJson)
+	return responseAsJson['user']['name']
 
 # request files
 def make_requester():
@@ -155,6 +199,7 @@ def make_requester():
 if __name__ == '__main__':
 	# retrieving absolute output directory
 	OUTPUTDIR = MAINDIR+OUTPUTDIR
+	get_config()
 	# creating main output directory, if needed
 	try:
 		os.stat(OUTPUTDIR)
@@ -174,20 +219,33 @@ if __name__ == '__main__':
 			try:
 				if DEBUG and EXTREME_DEBUG: pprint(f) # extreme debug
 				filename = f['name']
-				date = str(f['timestamp'])
+				timestamp = str(f['timestamp'])
 				user = users.get(f['user'], get_user_name(f['user']))
+				# isChannelMessageGroup = False
 				if len(f['channels']) > 0:
 					channel = get_channel_name(f['channels'][0])
+					channelId = f['channels'][0]['id']
 				elif len(f['groups']) > 0:
 					channel = get_group_name(f['groups'][0])
+					channelId = f['groups'][0]['id']
 				else:
 					print "No channel/group for file", f['id']
 					continue
+				# isChannelMessageGroup = channel.startsWith('mpdm-')
+				if channel not in CHANNELS: # and not isChannelMessageGroup
+					print "["+channel+"] File not in approved channels list", f['id']
+					continue
+				# if isChannelMessageGroup:
+				# 	channel = str(channel) + '-' + str(channelId)
 				file_url = f["url_private_download"]
+				file_id = f["id"]
 				basedir = OUTPUTDIR+'/'+channel
-				local_filename = get_local_filename(basedir, date, filename, user)
-				print "Downloading file '"+str(file_url)+"'"
-				download_file(file_url, local_filename, basedir)
+				local_filename = get_local_filename(basedir, filename, user, file_id, timestamp)
+				if os.path.exists(local_filename):
+					print "["+channel+"] File already exists - "+str(file_url)
+				else:
+					print "["+channel+"] Downloading file '"+str(file_url)+"'"
+					download_file(file_url, local_filename, basedir)
 				if ts == None or float(date) > float(ts): ts = date
 			except Exception, e:
 				if DEBUG: print str(e)
